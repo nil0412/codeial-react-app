bb9fc83a18a3b14fd749bca58bf4301a8c6023f0
Nilesh Patil <nil041297@gmail.com>
2023-03-22 11:53:41 +0530

    Added notifications to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,55 +1,57 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import { Navigate } from 'react-router-dom';
-// import { useToasts } from 'react-toast-notifications';
+import { useToasts } from 'react-toast-notifications';
 
 import styles from '../styles/login.module.css';
 // import { useAuth } from '../hooks';
 
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [loggingIn, setLoggingIn] = useState(false);
-//   const { addToast } = useToasts();
 //   const auth = useAuth();
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setLoggingIn(true);
-
-//     if (!email || !password) {
-//       return addToast('Please enter both email and password', {
-//         appearance: 'error',
-//       });
-//     }
-
-//     const response = await auth.login(email, password);
-
-//     if (response.success) {
-//       addToast('Successfully logged in', {
-//         appearance: 'success',
-//       });
-//     } else {
-//       addToast(response.message, {
-//         appearance: 'error',
-//       });
-//     }
-
-//     setLoggingIn(false);
-//   };
-
-//   if (auth.user) {
-//     return <Navigate to="/" />;
-//   }
-
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return addToast('Please enter both email and password', {
+        appearance: 'error',
+      });
+    }
+
+    // const response = await auth.login(email, password);
+
+    // if (response.success) {
+    //   addToast('Successfully logged in', {
+    //     appearance: 'success',
+    //   });
+    // } else {
+    //   addToast(response.message, {
+    //     appearance: 'error',
+    //   });
+    // }
+
+    // setLoggingIn(false);
+  };
+
+  // if (auth.user) {
+  //   return <Navigate to="/" />;
+  // }
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
         <input
           type="email"
           placeholder="Email"
-          isRequired
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
 
@@ -57,14 +59,13 @@ const Login = () => {
         <input
           type="password"
           placeholder="Paasword"
-          isRequired
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
 
-      <div className={styles.field}>
-        <button>
-          Log In
-        </button>
+      <div className={styles.field} disabled={loggingIn}>
+        <button>{loggingIn ? 'Logging in ...' : 'Log In'}</button>
       </div>
     </form>
   );
